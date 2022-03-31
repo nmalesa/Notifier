e5b185fc2053965766037de027a9ec4bee1903b9
Natalia Malesa <nmalesa@gmail.com>
2022-03-30 22:59:53 -0500

    Update PWA client for upload to Pages

--- notifier-client-pwa/src/App.js
+++ notifier-client-pwa/src/App.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 import './App.css';
 
 const httpUrl = 'https://notifier-rn-pwa.herokuapp.com';
-const wsURl = 'wss://notifier-rn-pwa.herokuapp.com';
+const wsUrl = 'wss://notifier-rn-pwa.herokuapp.com';
 
 let socket;
 
 const setUpWebSocket = addMessage => {
     if (!socket) {
         socket = new WebSocket(wsUrl);
-        console.log('Attempting connectio to socket...');
+        console.log('Attempting connection to socket...');
 
         socket.onopen = () => {
             console.log('Successfully connected to socket');
@@ -37,34 +37,33 @@ const loadInitialData = async setMessages => {
     console.log('Messages: ', messages.data);
 };
 
-const Item = ({ message }) => (
-    <div className="item">
-        <p className="item_message">{message}</p>
-    </div>
-)
+const App = () => {
+    const [messages, setMessages] = useState([]);
 
-function App() {
-  return (
-    <div>
-      <p>Hello, world!</p>
-    </div>
-  );
-}
+    useEffect(() => {
+        loadInitialData(setMessages);
+    }, []);
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-        backgroundColor: '#f9c2ff',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-    },
-    title: {
-        fontSize: 32,
-    },
-});
+    useEffect(() => {
+        setUpWebSocket(newMessage => {
+            setMessages([newMessage, ...messages]);
+        });
+    }, [messages]);
+
+    console.log(messages);
+
+    return (
+        <div>
+            <p>Test PWA</p>
+            <ul>
+                {messages.map(item => (
+                    <li key={item.id}>
+                        <div>{item.text}</div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
