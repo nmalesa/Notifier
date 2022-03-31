6463a243adc57ad25bcd241baef1b55dd8408776
Natalia Malesa <nmalesa@gmail.com>
2022-03-30 21:17:13 -0500

    Update React app with service worker template

--- notifier-client-pwa/src/App.js
+++ notifier-client-pwa/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-      <div>
-        <p>Is the manifest working?</p>
-      </div>
+    <div>
+      <p>Hello, world!</p>
+    </div>
   );
 }
 
